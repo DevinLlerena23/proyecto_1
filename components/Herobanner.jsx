5f2d5df0e4a825437d373517e9257df140dd3d6b
DevinLlerena23 <devinllerena@gmail.com>
2023-01-30 15:59:53 -0500

    Agregando productos

--- components/Herobanner.jsx
+++ components/Herobanner.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
 import { urlFor } from '../lib/Client';
+
 const HeroBanner = ({heroBanner}) => {
     return (
       <div className="hero-banner-container">
         <div>
           <p className="beats-solo">{heroBanner.smallText}</p>
           <h3>{heroBanner.midText}</h3>
-          <h4>{heroBanner.largeText}</h4>
-          <img src="" alt="Audifonos" className="hero-banner-image" />
-          <Link href="/product/ID">
-            <button type="button">BUTTON TEXT</button>
+          <h1>{heroBanner.largeText1}</h1>
+          <img src={urlFor(heroBanner.image)} alt="Audifonos" className="hero-banner-image" />
+
+          <Link href={`product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
             <h5>A bone le gusta el webo de Cesar</h5>
             <p>DESCRIPTION</p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,11 +11,11 @@ const Home = ({products, bannerData}) =>(
         console.log(bannerData)
       }
       <div className="products-heading">
-        <h2>Best Selling Products</h2>
-        <p>Speakers of many variations</p>
+        <h2>Productos más vendidos</h2>
+        <p>Audífonos de muchas variantes</p>
       </div>
       <div className="products-container">
-        {products?.map((product) => product.name 
+        {products?.map((product) => <Product key={product._id} product={product}/> 
         
         )}
       </div>
